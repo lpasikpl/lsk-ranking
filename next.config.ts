626f1119048b6ee56fc3347be4c99da2af10c1b9
Łukasz Pasikowski <lukaszpasikowski@MAC-LP.local>
2026-02-23 13:21:41 +0100

    next.config: dodaj supabase.co i graph.facebook.com do remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,14 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
       },
+      {
+        protocol: "https",
+        hostname: "*.supabase.co",
+      },
+      {
+        protocol: "https",
+        hostname: "graph.facebook.com",
+      },
     ],
   },
 };
